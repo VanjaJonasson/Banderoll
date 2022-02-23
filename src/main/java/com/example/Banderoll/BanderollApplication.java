67495d123801f9da0bdf9970a9d886665ceeddbf
Vanja Jonasson <vanja_j@hotmail.com>
2022-02-23 15:31:19 +0100

    first commit

---
+++ src/main/java/com/example/Banderoll/BanderollApplication.java
@@ -0,0 +1,15 @@
+package com.example.Banderoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanderollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanderollApplication.class, args);
+
+		System.out.println("hej");
+	}
+
+}
---
+++ src/test/java/com/example/Banderoll/BanderollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Banderoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanderollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
